de8567f6d02c8735434b5405fbefef73844938a4
Cyril Hou <houshoushuai@gmail.com>
2015-09-28 17:09:17 +0800

    [master] prepair for publish on lit

---
+++ package.lua
@@ -0,0 +1,19 @@
+--
+-- Created by Cyril Hou
+-- Date: 15/9/28
+-- Time: 2015-09-28 17:07:18
+--
+return {
+    name = "cyrilis/pinyin",
+    version = "0.0.1",
+    homepage = "https://github.com/cyrilis/lua-pinyin",
+    description = "Pinyin processer for lua or luvit lang",
+    tags = {"luvit", "pinyin", "拼音", "Chinese"},
+    license = "MIT",
+    author = { name = "Cyril Hou" },
+    files = {
+        "**.lua",
+        "**.md"
+    }
+}
+
